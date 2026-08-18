9aaf1fdd9d1cf2b9a1abd063c15c67deb1526604
John iiSU™ <cyberraggy2783@yahoo.com>
2026-08-17 21:58:06 -0500

    added more people

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord import app_commands
+import os
 
 class MyClient(discord.Client):
     def __init__(self):
@@ -11,14 +12,50 @@ class MyClient(discord.Client):
 
 client = MyClient()
 
+
+@client.tree.command(name="axleping", description="ping Axle")
+async def axleping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pinging Axle...", ephemeral=True)
+
+    for _ in range(15):
+        msg = await interaction.channel.send("<@1307916015589392455>")
+        await msg.delete()
+
+
+@client.tree.command(name="elginping", description="ping Elgin")
+async def elginping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pinging Elgin...", ephemeral=True)
+
+    for _ in range(15):
+        msg = await interaction.channel.send("<@1536205184223416431>")
+        await msg.delete()
+
+
 @client.tree.command(name="jadenping", description="ping le big J")
 async def jadenping(interaction: discord.Interaction):
-    await interaction.response.send_message("Starting...", ephemeral=True)
+    await interaction.response.send_message("Pinging Jaden...", ephemeral=True)
 
     for _ in range(15):
         msg = await interaction.channel.send("<@1489822967222243339>")
         await msg.delete()
 
-import os
+
+@client.tree.command(name="noahping", description="ping Noah")
+async def noahping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pinging Noah...", ephemeral=True)
+
+    for _ in range(15):
+        msg = await interaction.channel.send("<@974872649400463442>")
+        await msg.delete()
+
+
+@client.tree.command(name="robertping", description="ping Robert")
+async def robertping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pinging Robert...", ephemeral=True)
+
+    for _ in range(15):
+        msg = await interaction.channel.send("<@1538994823371952238>")
+        await msg.delete()
+
 
 client.run(os.environ["DISCORD_TOKEN"])
